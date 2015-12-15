1b97723150c74d1eb24ef36b4987425d68e43d41
thiagoleite86 <thiago.leite@nad1.com.br>
2015-12-15 13:17:26 -0300

    criação automatica de db.json

---
+++ app.js
@@ -0,0 +1,24 @@
+var jsonServer = require('json-server');
+
+var db = {};
+var files = fs.readdirSync(jsonfolder);
+files.forEach(function (file) {
+ if (path.extname(jsonfolder + file) === '.json') {
+     db[path.basename(jsonfolder + file, '.json')] = require(path.join(jsonfolder,file));
+ }
+});
+
+// Returns an Express server
+var server = jsonServer.create();
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(jsonServer.defaults());
+
+// Add custom routes
+// server.get('/custom', function (req, res) { res.json({ msg: 'hello' }) })
+
+// Returns an Express router
+var router = jsonServer.router(db);
+server.use(router);
+
+server.listen(3000);
